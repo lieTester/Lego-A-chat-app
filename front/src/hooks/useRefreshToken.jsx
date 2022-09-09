8c96047ee882489725288adbba63ee0f344d042a
puneet khandal <lietester.00@gmail.com>
2022-09-09 11:56:38 +0530

    commented console logs

--- front/src/hooks/useRefreshToken.jsx
+++ front/src/hooks/useRefreshToken.jsx
@@ -5,19 +5,23 @@ const useRefreshToken = () => {
   const { setauth } = useContext(AuthContext);
 
   const refreshToken = async () => {
-      console.log("Refreshing token...");
+    console.log("Refreshing token...");
     try {
-        const response = await axios.get("/api/auth/refresh-token/", {
-          withCredentials: true,
-        });
-        setauth((prev) => {
-          console.log(JSON.stringify(prev));
-          console.log(response);
-          return { ...prev, accessToken: response.data.token };
-        });
-        return response.data.accessToken;
+      const response = await axios.get("/api/auth/refresh-token/", {
+        withCredentials: true,
+      });
+      setauth((prev) => {
+        // console.log(JSON.stringify(prev));
+        console.log(response);
+        return {
+          ...prev,
+          accessToken: response.data.token,
+          username: response.data.username,
+        };
+      });
+      return response.data.accessToken;
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   };
 
