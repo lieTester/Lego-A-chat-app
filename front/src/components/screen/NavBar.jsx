3790ea1a3fe74a2891cb9878a27e0ec600424dc5
puneet khandal <lietester.00@gmail.com>
2022-09-11 12:19:45 +0530

    large device screen added

---
+++ front/src/components/screen/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <>
+    <div className="hidden sm:block">
+        big nav
+    </div>
+   
+    </>
+  )
+}
+
+export default NavBar
---
+++ front/src/components/screen/ScreenMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ScreenMain() {
+  return (
+    <div>ScreenMain</div>
+  )
+}
+
+export default ScreenMain
