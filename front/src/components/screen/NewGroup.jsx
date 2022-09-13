1325eff66d6f3f0f59c16891beee098bde879bd0
puneet khandal <lietester.00@gmail.com>
2022-09-13 12:40:48 +0530

    add and change screen components

--- front/src/components/screen/NewGroup.jsx
+++ front/src/components/screen/NewGroup.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function ContactsBox() {
+function NewGroup() {
   return (
     <div className="relative w-full h-full bg-prim1 sm:border-r-[2px] border-prim2 sm:w-[45%] md:w-[40%] lg:w-[30%] z-[2]">
-      ContactsBox
+      NewGroup
     </div>
   );
 }
 
-export default ContactsBox
+export default NewGroup
--- front/src/components/screen/SettingsBox.jsx
+++ front/src/components/screen/SettingsBox.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-function ContactsBox() {
+function SettingsBox() {
   return (
     <div className="relative w-full h-full bg-prim1 sm:border-r-[2px] border-prim2 sm:w-[45%] md:w-[40%] lg:w-[30%] z-[2]">
-      ContactsBox
+      SettingsBox
     </div>
   );
 }
 
-export default ContactsBox
+export default SettingsBox;
