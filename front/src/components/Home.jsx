4d25707ecae96eaf220b6efc29df8ffbb0668362
puneet khandal <lietester.00@gmail.com>
2022-09-13 12:20:53 +0530

    replace all the content on ScreenMain to home.jsx

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,39 +1,48 @@
-import {useContext,useEffect} from 'react'
-import { useNavigate, useLocation } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useNavigate, useLocation, Outlet } from "react-router-dom";
 import AuthContext from "../context/AuthProvider";
-import useAxiosPrivate from "../hooks/useAxiosPrivate"
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import MessagesBox from "./screen/MessagesBox";
+import NavBar from "./screen/NavBar";
+
 function Home() {
   const { auth } = useContext(AuthContext);
   const AxiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
 
-  useEffect(() => { 
-    let isMounted = true;
-    const controller = new AbortController();
+  // useEffect(() => {
+  //   let isMounted = true;
+  //   const controller = new AbortController();
 
-    const getUsers = async () => {
-      try {
-        const response = await AxiosPrivate.get("/api/auth/test", {
-          signal: controller.signal,
-        });
-        console.log(response.data);
-      } catch (err) {
-        console.error(err);
-        // navigate("/login", { state: { from: location }, replace: true });
-      }
-    };
+  //   const getUsers = async () => {
+  //     try {
+  //       const response = await AxiosPrivate.get("/api/auth/test", {
+  //         signal: controller.signal,
+  //       });
+  //       console.log(response.data);
+  //     } catch (err) {
+  //       console.error(err);
+  //       // navigate("/login", { state: { from: location }, replace: true });
+  //     }
+  //   };
 
-    getUsers();
+  //   getUsers();
 
-    return () => {
-      isMounted = false;
-      controller.abort();
-    };
-  },[]);
+  //   return () => {
+  //     isMounted = false;
+  //     controller.abort();
+  //   };
+  // },[]);
   console.log(auth);
   return (
-    <div className='min-h-screen bg-prim1'>Home</div>
-  )
+    <div className="relative h-screen  text-prim2 font-baloo ">
+      <NavBar />
+      <div className="relative h-[calc(100%-44px)] flex z-10">
+        <Outlet/>
+        <MessagesBox />
+      </div>
+    </div>
+  );
 }
-export default Home
+export default Home;
