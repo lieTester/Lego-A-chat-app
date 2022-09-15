a95fa7ff04239de38252c918655bad1f007eebaa
puneet khandal <lietester.00@gmail.com>
2022-09-15 12:06:11 +0530

    add msgBg as img and no actv chat dspl NoChatDspl

--- front/src/components/screen/MessagesBox.jsx
+++ front/src/components/screen/MessagesBox.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { BiSearch } from "react-icons/bi";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import user from "../../assets/images/user.png";
+import MessageDataContext from "../../context/MessageDataProvider";
+import NoChatDisplayId from "./screenSubComponents/NoChatDisplayId";
 function MessagesBox() {
-  const [zIndex, setZIndex] = useState(true);
   const [messages, setMessages] = useState();
+  const { messageData, setMessageData } = useContext(MessageDataContext);
   useEffect(() => {}, []);
-  const showMessages = (data,time,is_me) => {
+  const showMessages = (data, time, is_me) => {
     return (
       <div className={"flex" + (is_me ? " justify-end " : "")}>
         <ul>
@@ -19,20 +21,22 @@ function MessagesBox() {
     );
   };
 
-  return (
+  return messageData?.chat_id ? (
     <div
       className={
-        "absolute   w-full h-full bg-prim2 sm:relative  sm:w-[55%] md:w-[60%] lg:w-[70%] " +
-        (zIndex ? "z-[2]" : "z-[1]")
+        "absolute   w-full h-full bg-msg_bg bg-cover sm:relative  sm:w-[55%] md:w-[60%] lg:w-[70%] " +
+        (messageData.zIndex ? "z-[2]" : "z-[1]")
       }
     >
       <div className="relative h-[calc(100%-55px)] overflow-y-auto  pb-2 pt-[65px] text-prim1 text-[15px] [&>*]:px-4 [&>section>div>ul]:relative [&>section>div>ul]:w-fit  [&>section>div>ul]:px-1  [&>section>div>ul]:pt-1  [&>section>div>ul]:bg-seco2 [&>section>div>ul]:mb-2 [&>section>div>ul]:pr-4 [&>section>div>ul]:rounded-[3px] ">
-        <div className="absolute w-full h-[55px] top-0 bg-prim1">
+        <div className="fixed w-full h-[55px] top-[42px]  bg-prim1 z-[20] sm:w-[55%] md:w-[60%] lg:w-[70%]">
           <ul className="h-auto !w-full flex !p-0 !pt-1 !bg-transparent cursor-pointer">
             <li
               className=" sm:hidden mr-4 my-auto"
               onClick={() => {
-                setZIndex(!zIndex);
+                setMessageData((prev) => {
+                  return { ...prev, zIndex: false };
+                });
               }}
             >
               <BsFillArrowLeftCircleFill size={24} />
@@ -46,12 +50,31 @@ function MessagesBox() {
             </li>
             <li className=" relative w-[calc(100%-45px)] pl-4   [&>*]:block">
               <span className="absolute right-0 text-[12px]"></span>
-              <span className="text-prim1">Adi </span>
+              <span className="text-prim1">{messageData.name} </span>
               <span className="text-[13px]">online</span>
             </li>
           </ul>
         </div>
         <section>
+          {showMessages("Kha h", "2:10 pm", false)}
+          {showMessages("ghar pe", "2:10 pm", true)}
+          {showMessages("bhar chalke ate h", "2:11 pm", false)}
+          {showMessages("kaha gaya", "2:11 pm", false)}
+          {showMessages("yahi hu", "2:12 pm", true)}
+          {showMessages("man nahi h bad me", "2:12 pm", true)}
+          {showMessages("abhi kam h", "2:13 pm", true)}
+          {showMessages("5 min lagenge", "2:13 pm", false)}
+          {showMessages("me a raha hu tu chalega bs", "2:14 pm", false)}
+          {/*  */}
+          {showMessages("Kha h", "2:10 pm", false)}
+          {showMessages("ghar pe", "2:10 pm", true)}
+          {showMessages("bhar chalke ate h", "2:11 pm", false)}
+          {showMessages("kaha gaya", "2:11 pm", false)}
+          {showMessages("yahi hu", "2:12 pm", true)}
+          {showMessages("man nahi h bad me", "2:12 pm", true)}
+          {showMessages("abhi kam h", "2:13 pm", true)}
+          {showMessages("5 min lagenge", "2:13 pm", false)}
+          {showMessages("me a raha hu tu chalega bs", "2:14 pm", false)}
           {showMessages("Kha h", "2:10 pm", false)}
           {showMessages("ghar pe", "2:10 pm", true)}
           {showMessages("bhar chalke ate h", "2:11 pm", false)}
@@ -82,6 +105,8 @@ function MessagesBox() {
         </form>
       </div>
     </div>
+  ) : (
+    <NoChatDisplayId />
   );
 }
 
