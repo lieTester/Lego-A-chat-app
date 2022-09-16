2ef5d80e4009b33d01c58de10514ed06b68bdf29
puneet khandal <lietester.00@gmail.com>
2022-09-16 10:50:19 +0530

    message form functionality added with axios

--- front/src/components/screen/MessagesBox.jsx
+++ front/src/components/screen/MessagesBox.jsx
@@ -1,25 +1,47 @@
 import { useState, useEffect, useContext } from "react";
 import { BiSearch } from "react-icons/bi";
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import { BsFillArrowLeftCircleFill, BsThreeDotsVertical } from "react-icons/bs";
 import user from "../../assets/images/user.png";
 import MessageDataContext from "../../context/MessageDataProvider";
 import NoChatDisplay from "./screenSubComponents/NoChatDisplay";
+
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import MessageBlock from "./screenSubComponents/MessageBlock";
+
 function MessagesBox() {
-  const [messages, setMessages] = useState();
+  const AxiosPrivate = useAxiosPrivate();
+  const [typeMessage, setTypeMessage] = useState();
   const { messageData, setMessageData } = useContext(MessageDataContext);
-  useEffect(() => {}, []);
-  const showMessages = (data, time, is_me) => {
-    return (
-      <div className={"flex" + (is_me ? " justify-end " : "")}>
-        <ul>
-          <li className="absolute right-1 text-[10px] top-[-0.5px] text-prim2 ">
-            {time}
-          </li>
-          <li className="mr-2 mt-[2px]">{data}</li>
-        </ul>
-      </div>
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const response = await AxiosPrivate.post(
+      "/api/message/",
+      JSON.stringify({ chatid: messageData.chat_id, message: typeMessage })
     );
+    console.log(response);
+    if(response.status === 200) {
+      setTypeMessage('');
+    }
   };
+
+  useEffect(() => {
+    const getMessages = async () => {
+      // console.log(messageData.chat_id, "this");
+      const response = await AxiosPrivate.post(
+        "/api/message/get-messages",
+        JSON.stringify({ chatid: messageData.chat_id })
+      );
+      if (response) {
+        setMessageData((prev) => {
+          return { ...prev, chatMessages: response.data.chatMessages };
+        });
+      }
+    };
+
+    getMessages();
+    return () => {};
+  }, [messageData?.chat_id]);
 
   return messageData?.chat_id ? (
     <div
@@ -28,11 +50,11 @@ function MessagesBox() {
         (messageData.zIndex ? "z-[2]" : "z-[1]")
       }
     >
-      <div className="relative h-[calc(100%-55px)] overflow-y-auto  pb-2 pt-[65px] text-prim1 text-[15px] [&>*]:px-4 [&>section>div>ul]:relative [&>section>div>ul]:w-fit  [&>section>div>ul]:px-1  [&>section>div>ul]:pt-1  [&>section>div>ul]:bg-seco2 [&>section>div>ul]:mb-2 [&>section>div>ul]:pr-4 [&>section>div>ul]:rounded-[3px] ">
+      <div className="relative h-[calc(100%-55px)] overflow-y-auto  pb-2 pt-[65px] text-prim1 text-[15px] [&>*]:px-4 [&>section>div>ul]:relative [&>section>div>ul]:w-fit [&>section>div>ul]:px-1  [&>section>div>ul]:bg-seco2 [&>section>div>ul]:mb-2  ">
         <div className="fixed w-full h-[55px] top-[42px]  bg-prim1 z-[20] sm:w-[55%] md:w-[60%] lg:w-[70%]">
-          <ul className="h-auto !w-full flex !p-0 !pt-1 !bg-transparent cursor-pointer">
+          <ul className="h-auto !w-full flex !p-0 !pt-1 !bg-transparent ">
             <li
-              className=" sm:hidden mr-4 my-auto"
+              className=" sm:hidden mr-2 my-auto cursor-pointer"
               onClick={() => {
                 setMessageData((prev) => {
                   return { ...prev, zIndex: false };
@@ -45,54 +67,37 @@ function MessagesBox() {
               <img
                 src={user}
                 alt=""
-                className="!w-[42px] !h-[42px] rounded-full p-1"
+                className="!w-[42px] !h-[42px] rounded-full p-1 cursor-pointer"
               />
             </li>
-            <li className=" relative w-[calc(100%-45px)] pl-4   [&>*]:block">
+            <li className=" relative w-[calc(100%-100px)] pl-4   [&>*]:block sm:w-[calc(100%-40px)]">
               <span className="absolute right-0 text-[12px]"></span>
               <span className="text-prim1">{messageData.name} </span>
               <span className="text-[13px]">online</span>
             </li>
+            <li className="mt-3 cursor-pointer">
+              <BsThreeDotsVertical size={24} />
+            </li>
           </ul>
         </div>
         <section>
-          {showMessages("Kha h", "2:10 pm", false)}
-          {showMessages("ghar pe", "2:10 pm", true)}
-          {showMessages("bhar chalke ate h", "2:11 pm", false)}
-          {showMessages("kaha gaya", "2:11 pm", false)}
-          {showMessages("yahi hu", "2:12 pm", true)}
-          {showMessages("man nahi h bad me", "2:12 pm", true)}
-          {showMessages("abhi kam h", "2:13 pm", true)}
-          {showMessages("5 min lagenge", "2:13 pm", false)}
-          {showMessages("me a raha hu tu chalega bs", "2:14 pm", false)}
-          {/*  */}
-          {showMessages("Kha h", "2:10 pm", false)}
-          {showMessages("ghar pe", "2:10 pm", true)}
-          {showMessages("bhar chalke ate h", "2:11 pm", false)}
-          {showMessages("kaha gaya", "2:11 pm", false)}
-          {showMessages("yahi hu", "2:12 pm", true)}
-          {showMessages("man nahi h bad me", "2:12 pm", true)}
-          {showMessages("abhi kam h", "2:13 pm", true)}
-          {showMessages("5 min lagenge", "2:13 pm", false)}
-          {showMessages("me a raha hu tu chalega bs", "2:14 pm", false)}
-          {showMessages("Kha h", "2:10 pm", false)}
-          {showMessages("ghar pe", "2:10 pm", true)}
-          {showMessages("bhar chalke ate h", "2:11 pm", false)}
-          {showMessages("kaha gaya", "2:11 pm", false)}
-          {showMessages("yahi hu", "2:12 pm", true)}
-          {showMessages("man nahi h bad me", "2:12 pm", true)}
-          {showMessages("abhi kam h", "2:13 pm", true)}
-          {showMessages("5 min lagenge", "2:13 pm", false)}
-          {showMessages("me a raha hu tu chalega bs", "2:14 pm", false)}
+          {messageData?.chatMessages &&
+            messageData.chatMessages.map((data, index) => {
+              return <MessageBlock data={data} key={index} />;
+            })}
         </section>
       </div>
       <div className="absolute w-full bg-prim1 bottom-0 h-[55px] px-4  py-1">
-        <form action="" className="w-full py-1 ">
+        <form action="" className="w-full py-1 " onSubmit={sendMessage}>
           <ul className="relative flex border-[2px] p-[1px] border-prim2 rounded-full   ">
             <input
               type="text"
               id="chat-search"
               placeholder="send message . . ."
+              value={typeMessage}
+              onChange={(e) => {
+                setTypeMessage(e.target.value);
+              }}
               className="w-full outline-none bg-transparent pl-3 pr-1"
             />
             <button
