21908840828e67bae7996786072baa5b32d8f9e5
puneet khandal <lietester.00@gmail.com>
2022-09-16 11:25:42 +0530

    style in based on diffrent states

--- front/src/components/screen/screenSubComponents/NoChatDisplay.jsx
+++ front/src/components/screen/screenSubComponents/NoChatDisplay.jsx
@@ -2,12 +2,12 @@ import background from "../../../assets/images/lego-background.svg";
 function NoChatDisplay() {
   return (
     <div className="absolute w-full h-full font-bold  bg-msg_bg bg-cover sm:relative  sm:w-[55%] md:w-[60%] lg:w-[70%] ">
-      <ul className="text-lg [&>*]:mx-auto sm:mt-[50%] md:mt-[35%] lg:mt-[15%] xl:mt-[10%]">
+      <ul className="text-lg [&>*]:mx-auto sm:mt-[35%] md:mt-[20%] lg:mt-[15%] xl:mt-[10%]">
         <li className="font-caglisotro text-5xl flex justify-center">
           Namaste
         </li>
         <li className="text-prim1 flex justify-center text-3xl mt-4">
-          From <span className="text-seco2 mx-3 "> Lego</span> Family
+          From <span className="text-seco2 mx-3 "> Lego </span>Kutumb
         </li>
       </ul>
       <img
