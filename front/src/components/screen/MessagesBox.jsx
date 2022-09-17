ad89586f92a0128f956a775ea0f22d431ac10178
puneet khandal <lietester.00@gmail.com>
2022-09-17 21:06:06 +0530

    onload scrooll to bottom added..

--- front/src/components/screen/MessagesBox.jsx
+++ front/src/components/screen/MessagesBox.jsx
@@ -1,49 +1,107 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { BiSearch } from "react-icons/bi";
 import { BsFillArrowLeftCircleFill, BsThreeDotsVertical } from "react-icons/bs";
 import user from "../../assets/images/user.png";
 import MessageDataContext from "../../context/MessageDataProvider";
 import NoChatDisplay from "./screenSubComponents/NoChatDisplay";
-
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import MessageBlock from "./screenSubComponents/MessageBlock";
+import AuthContext from "../../context/AuthProvider";
+import io from "socket.io-client";
+
+// use state fluctuate so most recent chat id
+// if sender chat id and ours match, then we show to messageBox
+var socket, USER_CURRENT_CHAT_ROOM;
 
 function MessagesBox() {
   const AxiosPrivate = useAxiosPrivate();
-  const [typeMessage, setTypeMessage] = useState({value:''});
+  const messageBoxScroll = useRef();
+  const { auth } = useContext(AuthContext);
+  const [isLoding, setLoding] = useState(true);
+  const [typeMessage, setTypeMessage] = useState({ value: "" });
   const { messageData, setMessageData } = useContext(MessageDataContext);
 
-  const sendMessage = async (e) => {
-    e.preventDefault();
-    const response = await AxiosPrivate.post(
-      "/api/message/",
-      JSON.stringify({ chatid: messageData.chat_id, message: typeMessage })
-    );
-    console.log(response);
-    if(response.status === 200) {
-      setTypeMessage('');
-    }
-  };
-
+  // for messageBoxScroll
   useEffect(() => {
-    const getMessages = async () => {
-      // console.log(messageData.chat_id, "this");
-      const response = await AxiosPrivate.post(
-        "/api/message/get-messages",
-        JSON.stringify({ chatid: messageData.chat_id })
-      );
-      if (response) {
-        setMessageData((prev) => {
-          return { ...prev, chatMessages: response.data.chatMessages };
-        });
-      }
-    };
+    messageBoxScroll.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messageData?.chatMessages]);
 
-    getMessages();
+  // setting a socket connection
+  useEffect(() => {
+    socket = io(process.env.REACT_APP_END_POINT);
+    messageBoxScroll.current?.scrollIntoView({ behavior: "smooth" });
+    // socket.emit("user-online", auth.id);
+  }, []);
+
+  // setting a current on chat connection
+  useEffect(() => {
+    if (messageData.chat_id) {
+      USER_CURRENT_CHAT_ROOM = messageData.chat_id;
+      socket.emit("join-chat", messageData.chat_id);
+    }
+  }, [messageData?.chat_id]);
+
+  // reciving instant message for this chat
+  useEffect(() => {
+    socket.on("emit:new-message", (message) => {
+      if (USER_CURRENT_CHAT_ROOM === message.chatid) {
+        console.log(message.userid);
+      }
+    });
+  });
+
+  // for receiveing messages on load
+  useEffect(() => {
+    try {
+      const getMessages = async () => {
+        // console.log(messageData.chat_id, "this");
+        const response = await AxiosPrivate.post(
+          "/api/message/get-messages",
+          JSON.stringify({ chatid: messageData.chat_id })
+        );
+        if (response.status === 200) {
+          setLoding(false);
+          setMessageData((prev) => {
+            return { ...prev, chatMessages: response.data.chatMessages };
+          });
+        }
+      };
+      if (messageData.chat_id) {
+        // console.log("getting messageData");
+        getMessages();
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
     return () => {};
   }, [messageData?.chat_id, AxiosPrivate, setMessageData]);
 
-  return messageData?.chat_id ? (
+  // sending latest message data from user to server
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await AxiosPrivate.post(
+        "/api/message/",
+        JSON.stringify({
+          chatid: messageData.chat_id,
+          message: typeMessage.value,
+        })
+      );
+      if (response.status === 200) {
+        socket.emit("new-message", {
+          message: typeMessage.value,
+          chatid: messageData.chat_id,
+          userid: auth.id,
+        });
+        setTypeMessage({ value: "" });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  return isLoding ? (
+    <NoChatDisplay />
+  ) : (
     <div
       className={
         "absolute   w-full h-full bg-msg_bg bg-cover sm:relative  sm:w-[55%] md:w-[60%] lg:w-[70%] " +
@@ -80,12 +138,15 @@ function MessagesBox() {
             </li>
           </ul>
         </div>
+
         <section>
           {messageData?.chatMessages &&
             messageData.chatMessages.map((data, index) => {
               return <MessageBlock data={data} key={index} />;
             })}
         </section>
+
+        <section ref={messageBoxScroll} id="sample-view-for-scroll"></section>
       </div>
       <div className="absolute w-full bg-prim1 bottom-0 h-[55px] px-4  py-1">
         <form action="" className="w-full py-1 " onSubmit={sendMessage}>
@@ -95,9 +156,10 @@ function MessagesBox() {
               id="chat-search"
               placeholder="send message . . ."
               value={typeMessage.value}
+              autoComplete="off"
               onChange={(e) => {
                 e.preventDefault();
-                setTypeMessage({value:e.target.value});
+                setTypeMessage({ value: e.target.value });
               }}
               className="w-full outline-none bg-transparent pl-3 pr-1"
             />
@@ -111,8 +173,6 @@ function MessagesBox() {
         </form>
       </div>
     </div>
-  ) : (
-    <NoChatDisplay />
   );
 }
 
