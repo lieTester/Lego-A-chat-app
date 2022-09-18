af94a1ed3fef4a3243d5f2beecda9d587aa83e34
puneet khandal <lietester.00@gmail.com>
2022-09-18 14:50:07 +0530

    renaming of messageData->chatinfo

---
+++ front/src/context/ChatInfoProvider.jsx
@@ -0,0 +1,14 @@
+import { useState, createContext } from "react";
+
+const ChatInfoContext = createContext();
+export const ChatInfoProvider = ({ children }) => {
+  const [chatInfo, setChatInfo] = useState({});
+
+  return (
+    <ChatInfoContext.Provider value={{ chatInfo, setChatInfo }}>
+      {children}
+    </ChatInfoContext.Provider>
+  );
+};
+
+export default ChatInfoContext;
