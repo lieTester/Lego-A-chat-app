69e68c1f0ef05820c903e74c8a7a4f2fa9c4ffc4
puneet khandal <lietester.00@gmail.com>
2022-09-26 13:33:35 +0530

    added profile function route

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
   logout,
   forgotPassword,
   accessTokenTest,
+  profile,
 } = require("../Controllers/userControllers");
 
 const {
@@ -21,9 +22,10 @@ const {
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot", forgotPassword);
-router.get("/logout",  logout);
+router.get("/logout", logout);
 router.get("/refresh-token", refreshTokenVerification);
 router.post("/verify-OTP", OTPVerification, verifyOTP);
+router.post("/profile", accessTokenVerification, profile);
 router.get("/test", accessTokenVerification, accessTokenTest);
 
 module.exports = router;
