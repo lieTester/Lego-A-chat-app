05eef346eb3b70663e0071a891145d6e550a73dc
puneet khandal <lietester.00@gmail.com>
2022-09-28 18:12:49 +0530

    added profile maker

--- server/Utils/utilFunctions.js
+++ server/Utils/utilFunctions.js
@@ -3,10 +3,9 @@ const bcrypt = require("bcryptjs");
 
 // time string genaratior
 exports.getTime = (date) => {
-
   let todaysDate = new Date();
 
-  if(date.getDate() === todaysDate.getDate()){
+  if (date.getDate() === todaysDate.getDate()) {
     let hour = date.getHours() % 12 || 12;
     let minute = date.getMinutes() % 60 || 0;
     let isAm = date.getHours() < 12 ? "am" : "pm";
@@ -48,4 +47,39 @@ exports.decrypter = async (password, hash) => {
   const result = await bcrypt.compare(password, hash);
   // console.log(result);
   return result;
+};
+
+exports.makeAvatar = (name) => {
+  const colorGenerator = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    return "#" + randomColor;
+  };
+
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+
+  canvas.width = 200;
+  canvas.height = 200;
+
+  // Draw background
+  context.fillStyle = colorGenerator();
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Draw text
+  context.font = "bold 100px Assistant";
+  context.fillStyle = "white";
+  context.textAlign = "center";
+  context.textBaseline = "middle";
+  let letters;
+  const nameParts = name.split(" ");
+  if (nameParts.length > 1 && !nameParts[nameParts.length - 1].empty()) {
+    letters = nameParts[0][0] + nameParts[nameParts.length - 1][0];
+  } else {
+    letters = nameParts[0][0] + nameParts[0][1];
+  }
+  console.log(nameParts, letters);
+  context.fillText(letters, canvas.width / 2, canvas.height / 2);
+
+  return canvas.toDataURL("image/png");
 };
