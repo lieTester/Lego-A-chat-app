dda44be4a9b508d841b0c75082e362ce94d1e519
puneet khandal <lietester.00@gmail.com>
2022-10-02 17:24:08 +0530

    Date component added to highlite diffrent timeline of messages

--- front/src/components/screen/screenSubComponents/MessageBlock.jsx
+++ front/src/components/screen/screenSubComponents/MessageBlock.jsx
@@ -1,42 +1,48 @@
 import { useContext, useEffect } from "react";
 import ChatInfoContext from "../../../context/ChatInfoProvider";
+import Date from "./Date";
 
-function MessageBlock({ data }) {
+function MessageBlock({ data, change }) {
   const { chatInfo } = useContext(ChatInfoContext);
   useEffect(() => {
     // currentMsg.current.scrollToView();
   });
   return (
-    <div
-      className={
-        "w-full  flex " +
-        (data.is_me
-          ? "  justify-end [&>*]:rounded-l-[6px] [&>*]:rounded-tr-[6px] "
-          : " [&>*]:rounded-r-[6px] [&>*]:rounded-tl-[6px]  ")
-      }
-    >
-      <ul
+    <>
+      {change && <Date date={data.date} />}
+      <div
         className={
-          " !max-w-[70%]  overflow-hidden " +
-          (chatInfo.current_isGroup ? " !min-w-[70px] " : " !min-w-[40px] ")
+          "w-full  flex " +
+          (data.is_me
+            ? "  justify-end [&>*]:rounded-l-[6px] [&>*]:rounded-tr-[6px] "
+            : " [&>*]:rounded-r-[6px] [&>*]:rounded-tl-[6px]  ")
         }
       >
-        <li
+        <ul
           className={
-            "flex " +
-            (chatInfo.current_isGroup && !data.is_me ? "justify-between" : "justify-end")
+            " !max-w-[70%]  overflow-hidden " +
+            (chatInfo.current_isGroup ? " !min-w-[70px] " : " !min-w-[40px] ")
           }
         >
-          {chatInfo.current_isGroup && !data.is_me && (
-            <span className="text-prim3 mt-[-2px] font-bold text-[13px] mr-3">
-              {data.name}~
-            </span>
-          )}
-          <span className="text-[10px]  text-prim2  ">{data.time}</span>
-        </li>
-        <li className="mr-2 break-words mt-[-4px] ">{data.text}</li>
-      </ul>
-    </div>
+          <li
+            className={
+              "flex " +
+              (chatInfo.current_isGroup && !data.is_me
+                ? "justify-between"
+                : "justify-end")
+            }
+          >
+            {chatInfo.current_isGroup && !data.is_me && (
+              <span className="text-prim3 mt-[-2px] font-bold text-[13px] mr-3">
+                {data.name}~
+              </span>
+            )}
+            <span className="text-[10px]  text-prim2  ">{data.time}</span>
+          </li>
+          <li className="mr-2 break-words mt-[-4px] ">{data.text}</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
